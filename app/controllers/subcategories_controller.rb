8cc5803b4c00a50b1603e12c411f0264a88fae4d
heyjones <chris@heyjones.com>
2014-06-23 23:01:53 -0700

    modified subcategory index

--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -4,7 +4,7 @@ class SubcategoriesController < ApplicationController
   # GET /subcategories
   # GET /subcategories.json
   def index
-    @subcategories = Subcategory.all
+    @subcategories = Subcategory.where(category_id: params[:category_id])
   end
 
   # GET /subcategories/1
