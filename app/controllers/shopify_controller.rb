3c6e7bf401355d17f72b8f8de3fd93a6ebbcf791
heyjones <chris@heyjones.com>
2014-06-24 23:02:29 -0700

    added make to shopify model controller

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -12,8 +12,7 @@ class ShopifyController < ApplicationController
 
   def savings_calculator
   	@model = Model.first
-  	#@products = ShopifyAPI::Product.find(:all, :params => {:limit => 3})
-  	render :json => @model.to_json(:include => :model_products)
+  	render :json => @model.to_json(:include => [ :make, :model_products ])
   end
 
   def find_your_wheelset
