44db170e9ecae88c0554acfe33216258022c09ef
heyjones <chris@heyjones.com>
2014-06-25 00:34:39 -0700

    shhh

--- app/controllers/shopify_controller.rb
+++ app/controllers/shopify_controller.rb
@@ -11,12 +11,11 @@ class ShopifyController < ApplicationController
 	end
 
   def savings_calculator
-# 	if params.has_key?(:material_id)
-# 		@model = Model.where(:material_id => params[:material_id])
-# 	else
-# 		@model = Model.limit(1).order('random()')
-# 	end
-	@model = Model.first
+	if params.has_key?(:material_id)
+		@model = Model.first#where(:material_id => params[:material_id])
+	else
+		@model = Model.last#limit(1).order('random()')
+	end
   	render :json => @model.to_json(:include => [ :make, :model_products ])
   end
 
